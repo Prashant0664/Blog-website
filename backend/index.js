256c23c37eb82fd7507580c48df22b6a00cadba2
Thakran <prashantthakran0664@gmail.com>
2023-06-22 18:23:51 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 
 app.use(
   cors({
-    origin: ["http://localhost:5000", "http://localhost:3000"],
+    origin: ["https://allblogwebsiteapi.onrender.com/", "http://localhost:3000"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
