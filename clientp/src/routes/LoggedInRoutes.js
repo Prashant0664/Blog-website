0b9a2f22df2413f438bb655859deca5f5e48050a
prashant0664 <prashantthakran0664@gmail.com>
2024-05-31 23:31:42 +0530

    version client removed

--- clientp/src/routes/LoggedInRoutes.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
-
-export default function LoggedInRoutes() {
-  const { user } = useSelector((state) => ({ ...state }));
-
-  return user ? <Outlet /> : <Outlet />;
-}
--- clientp/src/routes/NotAllowedLoggedInRoutes.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function NotAllowedLoggedInRoutes() {
-  const { user } = useSelector((state) => ({ ...state }));
-
-  return user ? <Navigate to="/" /> : <Outlet />;
-}
