07ac9f8a13ce70917d40a0006c78bef1452accc5
prashant0664 <prashantthakran0664@gmail.com>
2024-09-28 15:14:15 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv").config();
-const Port = 5002;
+const Port = process.env.PORT || 5002;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
